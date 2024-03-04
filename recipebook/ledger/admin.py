c5ce6fc891a87dd75d61dc0c9920711deae342d7
Onym <jose.munda@student.ateneo.edu>
2024-03-04 22:42:05 +0800

    Uploaded Lab 2

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
 
-# Register your models here.
+
+class RecipeInLine(admin.StackedInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeInLine,]
+
+class RecipeIngredientInline(admin.StackedInline):  
+    model = RecipeIngredient
+
+class IngredientAdmin(admin.ModelAdmin):    
+    model = Ingredient
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
